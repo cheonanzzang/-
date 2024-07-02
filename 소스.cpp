ece516eea5c7e7266efd46ec002233314894aca1
cheonanzzang <kmkmkmim@gmail.com>
2024-07-02 16:50:50 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("æ»≥Á«œººø‰");
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	printf("æ»≥Á«œººø‰\næ»≥Á«œººø‰\næ»≥Á«œººø‰\n");
+	return 0;
+}
+
